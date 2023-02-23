1ef9c0bd0e5a7166125c05ac4cd1cf403a3d258f
@Youness_Elbrag <younsselbrag@gmail.com>
2023-02-23 14:24:30 +0100

    fix annotation syantx

--- server.cpp
+++ server.cpp
@@ -1,20 +1,18 @@
-#include <sys/socket.h>
-#include <isotream.h>
-#include <uinstd.h>
-#include <netinet/in.h>
-#include <cstdlib>
+#include<sys/socket.h>
+#include<uinstd.h>
+#include<netinet/in.h>
 
 using std::cout 
 using std::endl
-using std::string
+using std::striing
 
 int main(){
-   // Create a socket struct MyStruc
+  // Create a socket struct MyStruc
    int sockets = socket(AF_INET,SOCK_STREAM,0);
    if(sockets == -1 ){
      cout<<"the connection is failed"<< erron <<endl;
      exit(EXIT_FAILURE)
-   }
+   };
    // listing to the port 
    sockaddr_in sockaddr;
    sockaddr.sin_family = AF_INET;
@@ -25,29 +23,28 @@ int main(){
      cout<<"Failed to bind to the port 8000 erron :"<< erron <<endl;
      exit(EXIT_FAILURE);
 
-   }
+   };
   if(listen(sockets,10)<0){
 
      cout<<"Failed to grab connection erron:"<< erron <<endl;
      exit(EXIT_FAILURE);
-   }
+   };
    // Grab thnnection from queue 
   auto addrlen = sizeof(sockaddr);
   int connection = accept(sockets,(struct sockaddr*)&sockaddr.(socklen_t*)&addrlen);
   if(connection < 0){
-
-     cout<<Failed to grab the connection erron :"<< erron <<endl;
-     exit(EXIT_FAILURE);
-   }
-   // read from the buffer 
+      cout<<"Failed to grab the connection erron :"<< erron <<endl;
+      exit(EXIT_FAILURE);
+   };
+   /* read from the buffer */
   char buffer[100];
   auto bytesRead = read(connection , buffer,100);
      cout<<"the message was: "<< buffer <<endl; 
-   // send a message 
+   /* send a message */
   string respond = "hey there nice to have you\n";
   send(connection. respond.c_str(),respond.size(),0);
 
-   // close the connectioo
+   /* close the connectioo */
   close(connection);
   close(sockets);
   }
